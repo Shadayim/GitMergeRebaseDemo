3cf1cdff2d54e7dca08ac30f8664fd9fbd2ce27c
Nico Bataillie <nico.bataillie@allphi.eu>
2024-10-09 14:53:59 +0200

    [Feature 2] Meaningful commit message

--- GitMergeRebaseDemo/Developer2.cs
+++ GitMergeRebaseDemo/Developer2.cs
@@ -2,5 +2,11 @@
 
 public class Developer2
 {
-    
+    // Developer2 is adding a new feature here
+    // oops I forgot to commit a file
+    // let me fix that typo
+    // and another typo fixed
+    // time to go home, let me commit my work
+    // Yay I finished work on this feature
+    // fix failing tests
 }
