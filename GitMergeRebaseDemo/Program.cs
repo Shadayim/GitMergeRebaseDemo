937645c26f2350eb881bae594ff0fd83f5904a76
Nico Bataillie <nico.bataillie@allphi.eu>
2024-10-09 14:39:53 +0200

    [BugFix] Clear description of the bug that was fixed

--- GitMergeRebaseDemo/Program.cs
+++ GitMergeRebaseDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hi there! This is a new line of code!");
+Console.WriteLine("My code had a bug, but now it's fixed!");
+
+
 
