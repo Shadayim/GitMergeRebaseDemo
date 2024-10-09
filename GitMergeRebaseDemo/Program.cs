805fafb983b59726b7eac9518003d0903e0208c4
Nico Bataillie <nico.bataillie@allphi.eu>
2024-10-09 15:02:33 +0200

    [Feature] More good work being done

--- GitMergeRebaseDemo/Program.cs
+++ GitMergeRebaseDemo/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Let's keep developing more features");
+Console.WriteLine("Let's keep up the good work!!!");
 
 
 
